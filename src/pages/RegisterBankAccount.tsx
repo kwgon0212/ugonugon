4bae78a359a396325c87da5a8b21df381067c721
U_goni <86401186+kwgon0212@users.noreply.github.com>
2025-02-26 13:34:25 +0900

    feat: import 절대경로 설정 셋팅

--- src/pages/RegisterBankAccount.tsx
+++ src/pages/RegisterBankAccount.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import Header from "../components/Header";
-import Main from "../components/Main";
+import React, { useRef } from "react";
+import Header from "@/src/components/Header";
 import BottomNav from "../components/BottomNav";
-import ArrowLeftIcon from "../components/icons/ArrowLeft";
+import Main from "../components/Main";
 
 const RegisterBankAccount = () => {
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+
+  const handleSelectBank = () => {
+    if (!modalRef.current) return;
+    modalRef.current.classList.add("modal-open");
+  };
+
   return (
     <>
       <Header>
@@ -16,38 +22,22 @@ const RegisterBankAccount = () => {
           <div className="flex flex-col gap-layout">
             <input
               placeholder="계좌번호를 입력해주세요"
-              className="h-[50px] rounded-[10px] px-layout"
+              className="h-[50px] rounded-[10px] px-layout border border-main-gray outline-main-color"
             />
-            <button className="w-full h-[50px] rounded-[10px] px-layout bg-white text-main-gray text-left">
-              <span>은행선택</span>
-            </button>
             {/* You can open the modal using document.getElementById('ID').showModal() method */}
             <button
-              className="btn"
-              // onClick={() => document.getElementById("my_modal_3").showModal()}
+              className="w-full h-[50px] rounded-[10px] px-layout bg-white text-main-gray text-left"
+              onClick={handleSelectBank}
             >
-              open modal
+              <span>은행선택</span>
             </button>
-            <dialog id="my_modal_3" className="modal">
-              <div className="modal-box">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                    ✕
-                  </button>
-                </form>
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">
-                  Press ESC key or click on ✕ button to close
-                </p>
-              </div>
-            </dialog>
-            <ArrowLeftIcon width={50} height={50} color="#000000" />
           </div>
         </div>
       </Main>
       <BottomNav>
-        <></>
+        <button className="w-full h-[50px] rounded-[10px] px-layout bg-white text-main-gray text-left">
+          다음
+        </button>
       </BottomNav>
     </>
   );
