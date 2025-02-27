0347f7dfc08abb7a6f90c7dbf2dacefe9d5f35d7
U_goni <86401186+kwgon0212@users.noreply.github.com>
2025-02-27 10:40:51 +0900

    feat: redux 셋팅

--- util/store.ts
+++ util/store.ts
@@ -1,7 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import UserInfo from "@/types/UserInfo";
+
+const userInitialState: UserInfo = {
+  _id: "",
+  name: "",
+  사업자번호: "",
+  sex: "",
+  주민번호: "",
+  phone: "",
+  address: {
+    zip: "",
+    mainAddress: "",
+    subAddress: "",
+  },
+  sign: {},
+  bankAccount: {
+    bank: "",
+    account: "",
+  },
+  email: "",
+  password: "",
+};
+
+const user = createSlice({
+  name: "user",
+  initialState: userInitialState,
+  reducers: {},
+});
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: user.reducer,
+  },
 });
 
 export default store;
