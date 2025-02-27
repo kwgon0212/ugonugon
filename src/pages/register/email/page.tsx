63eced98efbb83ad7df4068b537af229754e5595
wonder1ng <wonder1ng@velog.io>
2025-02-27 15:16:08 +0900

    chore: 최상위 div를 form으로 변경, BottomButton 위치 조정

--- src/pages/register/email/page.tsx
+++ src/pages/register/email/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import Header from "../../../components/Header";
 import Main from "../../../components/Main";
-import BottomNav from "../../../components/BottomNav";
 import ArrowLeftIcon from "../../../components/icons/ArrowLeft";
 import CancelIcon from "../../../components/icons/Cancel";
 
@@ -21,7 +20,11 @@ const InsertTextInput = styled.input`
 `;
 
 const BottomButton = styled.button`
-  width: 100%;
+  position: absolute;
+  bottom: 60px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: calc(100% - 40px);
   height: 50px;
   border-radius: 10px;
   font-size: 14px;
@@ -39,8 +42,8 @@ function RegisterEmailPage() {
           {/* <hr /> */}
         </div>
       </Header>
-      <Main hasBottomNav={true}>
-        <div className="w-full p-layout flex flex-col gap-layout divide-[#0b798b]">
+      <Main hasBottomNav={false}>
+        <form className="w-full p-layout flex flex-col gap-layout divide-[#0b798b]">
           <p className="font-semibold text-xl">이메일 인증</p>
           <p className="text-main-darkGray">
             원활한 서비스 이용을 위한
@@ -48,17 +51,15 @@ function RegisterEmailPage() {
             계정에 사용할 이메일 계정을 입력해주세요.
           </p>
           <InsertTextInput
-            type="text"
+            type="email"
             placeholder="이메일 계정"
             className="w-full h-[50px] bg-white rounded-[10%]"
+            pattern="[\w]+@+[\w]+\.[\w]+"
+            required
           />
-        </div>
-      </Main>
-      <BottomNav>
-        <div className="pb-15 px-5">
           <BottomButton>인증번호 전송</BottomButton>
-        </div>
-      </BottomNav>
+        </form>
+      </Main>
     </>
   );
 }
