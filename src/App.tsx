94d844dc0903666df0dc9c713a99fb7cc88fc2fb
U_goni <86401186+kwgon0212@users.noreply.github.com>
2025-02-28 13:12:36 +0900

    feat: 페이지 이동할 때 모션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import MyPage from "./pages/mypage/page";
 import ReCruitPage from "./pages/recruit/page";
 import MapPage from "./pages/map/page";
 import RegisterInfoPage from "./pages/register/Info/page.jsx";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
@@ -25,28 +26,30 @@ function App() {
         <Aside />
         {/* 모바일 레이아웃 */}
         <div className="relative max-w-[560px] w-full h-screen mx-auto lg:mx-0">
-          <Routes>
-            <Route path="/" element={<RootPage />} />
-            <Route path="/register">
-              <Route path="sign" element={<RegisterSign />} />
-              <Route path="address" element={<RegisterAddress />} />
-              <Route path="email" element={<RegisterEmailPage />} />
-              <Route path="email/cert" element={<RegisterEmailCertPage />} />
-              <Route path="bank-account" element={<RegisterBankAccount />} />
-              <Route path="user-account" element={<RegisterUserAccount />} />
-              <Route path="info" element={<RegisterInfoPage />} />
-              <Route path="business-num" element={<RegisterBusinessPage />} />
-            </Route>
-            <Route path="/recruit" element={<ReCruitPage />} />
-            <Route path="/map" element={<MapPage />} />
-            <Route path="/notice">
-              <Route path=":id" element={<NoticeDetailPage />} />
-              <Route path="list" element={<NoticeListPage />} />
-            </Route>
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/work" element={<WorkPage />} />
-          </Routes>
+          <AnimatePresence>
+            <Routes>
+              <Route path="/" element={<RootPage />} />
+              <Route path="/register">
+                <Route path="sign" element={<RegisterSign />} />
+                <Route path="address" element={<RegisterAddress />} />
+                <Route path="email" element={<RegisterEmailPage />} />
+                <Route path="email/cert" element={<RegisterEmailCertPage />} />
+                <Route path="bank-account" element={<RegisterBankAccount />} />
+                <Route path="user-account" element={<RegisterUserAccount />} />
+                <Route path="info" element={<RegisterInfoPage />} />
+                <Route path="business-num" element={<RegisterBusinessPage />} />
+              </Route>
+              <Route path="/recruit" element={<ReCruitPage />} />
+              <Route path="/map" element={<MapPage />} />
+              <Route path="/notice">
+                <Route path=":id" element={<NoticeDetailPage />} />
+                <Route path="list" element={<NoticeListPage />} />
+              </Route>
+              <Route path="/mypage" element={<MyPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/work" element={<WorkPage />} />
+            </Routes>
+          </AnimatePresence>
         </div>
       </div>
     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,29 @@
 import React, { JSX } from "react";
+import { motion } from "framer-motion";
+
 type Props = {
   children: JSX.Element;
   hasBottomNav: boolean;
 };
+
+const pageVariants = {
+  initial: { opacity: 0, x: "-10px" },
+  animate: { opacity: 1, x: 0 },
+  // exit: { opacity: 0, x: "10px" },
+};
+
 const Main = ({ children, hasBottomNav }: Props) => {
   return (
-    <main
+    <motion.main
+      variants={pageVariants}
+      initial="initial"
+      animate="animate"
       className={`absolute bg-main-bg top-16 ${
         hasBottomNav ? "bottom-[80px]" : "bottom-0"
       } w-full overflow-y-auto overflow-x-hidden scrollbar-hidden`}
     >
       {children}
-    </main>
+    </motion.main>
   );
 };
 
