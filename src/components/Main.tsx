1a7562d3f2c88b62965efc589a46b57bc3a8f94e
U_goni <86401186+kwgon0212@users.noreply.github.com>
2025-03-03 17:49:30 +0900

    chore: 공고 링크 복사 기능 추가

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,29 +1,25 @@
 import React, { JSX } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {
   children: JSX.Element;
   hasBottomNav: boolean;
 };
 
-const pageVariants = {
-  initial: { opacity: 0, x: "-10px" },
-  animate: { opacity: 1, x: 0 },
-  // exit: { opacity: 0, x: "10px" },
-};
-
 const Main = ({ children, hasBottomNav }: Props) => {
   return (
+    // <AnimatePresence mode="wait">
     <motion.main
-      variants={pageVariants}
-      initial="initial"
-      animate="animate"
+      initial={{ x: 200, opacity: 1 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ duration: 0.1, ease: "linear" }}
       className={`absolute bg-main-bg top-16 ${
         hasBottomNav ? "bottom-[80px]" : "bottom-0"
       } w-full overflow-y-auto overflow-x-hidden scrollbar-hidden`}
     >
       {children}
     </motion.main>
+    // </AnimatePresence>
   );
 };
 
