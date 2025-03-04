9a75dd01328a87ee7ba03af3d321e06e6fa27197
U_goni <86401186+kwgon0212@users.noreply.github.com>
2025-03-04 10:23:39 +0900

    feat: swagger api docs 생성

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import testRoutes from "./routes/test.ts";
+import { setupSwagger } from "../swagger/swagger.ts";
 dotenv.config();
 
 const app: Express = express();
@@ -9,6 +10,7 @@ const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+setupSwagger(app);
 
 app.use("/api/test", testRoutes);
 
@@ -16,6 +18,29 @@ app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}에서 서버 구동중...`);
 });
 
+/**
+ * @swagger
+ * /api:
+ *   get:
+ *     summary: 유저 목록 조회
+ *     description: 모든 유저를 조회하는 API
+ *     responses:
+ *       200:
+ *         description: 성공적으로 유저 목록을 반환함
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "Jaehyun"
+ */
 app.get("/api", (req: Request, res: Response) => {
   res.status(200).json({ message: "root" });
 });
