16e5fc8b04f8575a220e0e43fdedbd2adc1224ab
U_goni <86401186+kwgon0212@users.noreply.github.com>
2025-03-04 16:21:59 +0900

    feat: mongoDB 연결

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import testRoutes from "./routes/test.ts";
 import { setupSwagger } from "../swagger/swagger.ts";
+import mongoose from "mongoose";
 dotenv.config();
 
 const app: Express = express();
@@ -10,11 +11,16 @@ const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 setupSwagger(app);
 
 app.use("/api/test", testRoutes);
 
 app.listen(PORT, () => {
+  mongoose
+    .connect(process.env.MONGO_DB_URI as string)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
   console.log(`http://localhost:${PORT}에서 서버 구동중...`);
 });
 
